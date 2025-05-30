e9e4536a6b5d0acf8e559dbebb8422c697df0def
kino1022 <yuta_fujita@outlook.com>
2025-05-30 15:42:27 +0900

    Update RotateManager.cs

--- Assets/Project/Script/Camera/RotateManager.cs
+++ Assets/Project/Script/Camera/RotateManager.cs
@@ -9,6 +9,13 @@ namespace Project.Script.Camera {
         [SerializeField] protected ITargetHolder<Transform> m_target;
 
         [SerializeField] protected ITargetHolder<Transform> m_maseter;
+
+        [SerializeField] protected float m_angleLimit = 60.0f;
+        
+        /// <summary>
+        /// 例外発生時に向くデフォルトの方向
+        /// </summary>
+        [SerializeField] protected Vector3 m_defaultDirection = new Vector3(1.0f, 0.0f, 0.0f);
         
         //-------------------API Methods----------------------------------
 
@@ -18,12 +25,44 @@ namespace Project.Script.Camera {
         
         //------------------Logical methods-------------------------------
 
-        public Quaternion CalculateAngle() {
-            return Quaternion.LookRotation(CalculateDirection());
+        protected Quaternion CalculateAngle() {
+            var direction = CalculateDirection();
+            
+            //ゼロベクトルならデフォルトの方向を向く
+            if (direction == Vector3.zero) {
+                return Quaternion.LookRotation(m_defaultDirection);
+            }
+            
+            return Quaternion.LookRotation(direction);
         }
 
-        public Vector3 CalculateDirection() {
-            return m_target.GetTarget().position - m_maseter.GetTarget().position;
+        protected Vector3 CalculateDirection() {
+            var target = m_target.GetTarget().position;
+            var master = m_maseter.GetTarget().position;
+            
+            //対象がいるかどうかのチェック
+            if (target == null || master == null || m_target.GetTarget() == null || m_maseter.GetTarget() == null) {
+                return m_defaultDirection;//ひっかかった場合はデフォルト値を返す
+            }
+            
+            //ターゲットとマスターの座標不明悪霊殺しが一緒ならデフォルト値を返す
+            if (target == master) {
+                return m_defaultDirection;
+            }
+            
+            var result = target - master;
+            result = OnCalculateDirection(result);
+            return result;
         }
+
+        protected Vector3 OnCalculateDirection(Vector3 direction) {
+            return ApplicableAngleLimit(direction);
+        }
+
+        protected Vector3 ApplicableAngleLimit(Vector3 direction) {
+            var result = direction;
+            return result;
+        }
+        
     }
 }
