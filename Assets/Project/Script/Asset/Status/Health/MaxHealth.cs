486fa87d091cc213328260c4eefa436ab752fa77
kino1022 <yuta_fujita@outlook.com>
2025-06-05 14:14:52 +0900

    delete oldStatus

--- Assets/Project/Script/Asset/Status/Health/MaxHealth.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Project.Script.Status;
-
-namespace Project.Script.Asset.Status.Health {
-    [Serializable]
-    public class MaxHealth : CorrectableStatus {
-        
-    }
-}
--- Assets/Project/Script/Status/BaseValueElement.cs
+++
@@ -1,7 +0,0 @@
-
-namespace Project.Script.Status {
-    [System.Serializable]
-    public class BaseValueElement : AStatusElement {
-        public BaseValueElement(float value) : base(value) { }
-    }
-}
