65f2067b3f84930a89861d542bd4d550697b6427
kino1022 <yuta_fujita@outlook.com>
2025-07-16 18:38:28 +0900

    Update ICancelMotionContext.cs

--- Assets/Project/Script/Motion/State/Context/Interface/ICancelMotionContext.cs
+++ Assets/Project/Script/Motion/State/Context/Interface/ICancelMotionContext.cs
@@ -3,9 +3,6 @@ using UnityEngine;
 namespace Teiwas.Script.Motion.State.Context.Interface {
     public interface ICancelMotionContext {
         public void Update(int currentFrame);
-
         public bool Cancelable(AnimationClip motion);
-
-        
     }
 }
